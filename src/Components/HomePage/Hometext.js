19e10f441376c17a4986f47d92b73b5933363299
SananM <smuzefferov1@gmail.com>
2022-12-14 02:29:03 +0400

    homepage finished

---
+++ src/Components/HomePage/Hometext.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomePage = () => {
+    return(
+        <div className="home">
+            <div className="homeText">
+                <h2>Make a Wish</h2>
+                <p>We would like to help you send a message <br/> to someone you care about</p>
+                <button type="button">Start the magic</button>
+            </div>
+        </div>
+    );
+}
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import HomePage from './Components/HomePage/Hometext';
+import FormBlank from './Components/FormPage/FormBlank';
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 const App = () => {
   return(
     <div>
-
+      <HomePage />
+      <FormBlank />
     </div>
   );
 }
