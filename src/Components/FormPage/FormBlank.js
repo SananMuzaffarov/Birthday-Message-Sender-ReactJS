3213d46646251f403667826e5ed9826506ceac01
SananM <smuzefferov1@gmail.com>
2022-12-14 03:48:40 +0400

    form updated

--- src/Components/FormPage/FormBlank.js
+++ src/Components/FormPage/FormBlank.js
@@ -3,7 +3,21 @@ import React from "react";
 const FormBlank = () => {
     return(
         <div className="formBlank">
-
+            <div className="col col1">
+                <p>Write a wish for someone you care. Add <br /> their name and yours, create the wish and share it!</p>
+            </div>
+            <div className="col">
+                <form>
+                    <label>Who are you writing a wish for?</label>
+                    <input type="text" placeholder="Name"/>
+                    <br />
+                    <label>From</label>
+                    <input type="text" placeholder="Your name" />
+                    <br />
+                    <textarea placeholder="Your wish" />
+                    <button type="submit">Create wish</button>
+                </form>
+            </div>
         </div>
     );
 }
