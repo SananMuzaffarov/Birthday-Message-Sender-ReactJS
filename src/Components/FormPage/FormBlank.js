68d39fdab45f8dcef6001f2c118141d7ca3b9354
SananM <smuzefferov1@gmail.com>
2022-12-15 02:44:33 +0400

    nearly finished

--- src/Components/FormPage/FormBlank.js
+++ src/Components/FormPage/FormBlank.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
 
 const FormBlank = () => {
+
+   const form = useRef()
+
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_v3ze9ha', 'template_fgmfgmo', form.current, 'Z3ARLYuvWTQ117QFq')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset()
+    };
+
     return(
         <div className="formBlank">
             <div className="col col1">
                 <h3>Write a wish for someone you care. Add <br /> their name and yours, create the wish and <br/> share it!</h3>
             </div>
             <div className="col col2">
-                <form>
-                    <label>Who are you writing a wish for?</label>
-                    <input type="text" placeholder="Name"/>
-                    <br />
+                <form ref={form} onSubmit={handleSubmit}>
                     <label>From</label>
-                    <input type="text" placeholder="Your name"/>
+                    <input type="text" placeholder="Your name" required name="user_name"/>
                     <br />
-                    <textarea placeholder="Your wish" type="text"/>
+                    <label>Subject</label>
+                    <input type="text" placeholder="Your Subject" required name="subject"/>
+                    <br />
+                    <label>Who are you writing a wish for?</label>
+                    <input type="email" placeholder="Email" required name="user_email"/>
+                    <br />
+                    <textarea placeholder="Your wish" type="text" required/>
                     <button type="submit">Create wish</button>
                 </form>
             </div>
